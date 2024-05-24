3b5c194254b65690b43e84338f13d51d09f296f0
ailicic <ailicic@PD-AILICIC.local>
2024-05-24 15:17:47 +0200

    adding vnet to test CI

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,12 @@ resource "azurerm_resource_group" "rg-aleksandar-ilicic" {
   name     = "rg-aleksandar-ilicic"
 }
 
+
+resource "azurerm_virtual_network" "vnet" {
+
+    name                = "vnet-aleksandar-ilicic"
+    address_space       = ["10.0.0.1/24"]
+    location            = azurerm_resource_group.rg-aleksandar-ilicic.location
+    resource_group_name = azurerm_resource_group.rg-aleksandar-ilicic.name
+
+}
