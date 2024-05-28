c004f9609c1c0427b1f1e07153ada39c2f1ca435
ailicic <ailicic@PD-AILICIC.local>
2024-05-28 11:31:46 +0200

    testing git

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,35 @@ resource "azurerm_virtual_network" "vnet" {
     resource_group_name = azurerm_resource_group.rg-aleksandar-ilicic.name
 
 }
+
+resource "azurerm_virtual_machine" "test" {
+    name                  = "test"
+    resource_group_name   = azurerm_resource_group.rg-aleksandar-ilicic.name
+    location              = azurerm_resource_group.rg-aleksandar-ilicic.location
+    network_interface_ids = [azurerm_network_interface.nic.id]
+
+    storage_os_disk {
+        name              = "osdisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Standard_LRS"
+    }
+
+    os_profile {
+        computer_name  = "test"
+        admin_username = "adminuser"
+        admin_password = "password"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
+
+ 
+
+    vm_size = "Standard_DS1_v2" # Add the missing "vm_size" attribute
+
+    tags = {
+        environment = "dev"
+    }
+}
